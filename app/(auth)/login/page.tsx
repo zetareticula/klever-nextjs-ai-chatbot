5111b55d07e39c5f8280d23d599cc0673160baed
Zeta Reticula <zetareticulae@proton.me>
2025-02-25 20:58:11 -0800

    Remove the empty line between your import statements on line 4
    Add an empty line between different import groups on line 11

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,7 +2,6 @@
 
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-
 import { useEffect, useState } from "react";
 import { useFormState, useFormStatus } from "react-dom";
 import { toast } from "sonner";

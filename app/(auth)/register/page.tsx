e02ae6fa1402d3149fe56fbfb287e04884b0d8bc
Zeta Reticula <zetareticulae@proton.me>
2025-02-27 19:40:49 -0800

    next/link now comes before next/navigation
    One empty line between different import groups:

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
